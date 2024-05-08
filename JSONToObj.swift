3b4e0b121a8944d6eef43c82ab215edcf17ce021
Ted Neward <ted@tedneward.com>
2024-05-07 23:49:34 -0700

    Update JSONToObj.swift

--- JSONToObj.swift
+++ JSONToObj.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+// {{## BEGIN swift-types ##}}
 let jsonData = 
   ("[{\"lastName\":\"Neward\",\"firstName\":\"Ted\"}," + 
    "{\"lastName\":\"Neward\",\"firstName\":\"Charlotte\"}]")
@@ -7,3 +8,17 @@ let jsonData =
 
 let people = try JSONSerialization.jsonObject(with: jsonData!)
 print(people)
+// {{## END swift-types ##}}
+
+// {{## BEGIN codable-decoding ##}}
+struct Person : Codable {
+    let firstName: String
+    let lastName: String
+    //let favoritePhrases: Array<String>
+        // this will trip an error if there isn't a field in the JSON
+}
+
+let decoder = JSONDecoder()
+let persons = try decoder.decode(Array<Person>.self, from: jsonData!)
+print(persons)
+// {{## END codable-decoding ##}}
